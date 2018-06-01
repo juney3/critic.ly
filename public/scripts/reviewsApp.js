c5d74e5b610c6e1f9184c7b52555c2a6e8975910
juneyoshii <juney37@gmail.com>
2018-06-01 03:32:31 -0700

    file cleanup

--- public/scripts/reviewsApp.js
+++ public/scripts/reviewsApp.js
@@ -183,19 +183,14 @@ function getReviewData(movie) {
     if (reviewsRatings.reviews != "No reviews currently") {
       renderAllReviews(reviewsRatings);
     }
-    console.log('movie rendered');
   }
 
   //Delete review
   $('.movieReviews').on('submit', function(event){
     event.preventDefault();
 
-    console.log(this);
-    console.log(event.target)
     let id = $(event.target).data('id');
-    console.log(id);
     let deleteURL = `/api/reviews/${id}`
-    console.log(deleteURL)
     $.ajax({
       method: 'DELETE',
       url: `/api/reviews/${id}`,
@@ -256,8 +251,6 @@ Submit functions
 
     //Open a movie from one of the all movies buttons
     $('#allMovies').on('click', function(event){
-      console.log(this);
-      console.log(event.target);
       let movieId=$('.oneMovie').data('id');
 
       $.ajax({
