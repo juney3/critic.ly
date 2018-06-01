ddd48e3e8def347321cc660a637f0aa70f67a4e7
juneyoshii <juney37@gmail.com>
2018-05-31 22:44:01 -0700

    minor styling changes to myReviews

--- public/scripts/reviewsApp.js
+++ public/scripts/reviewsApp.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   let reviewText;
   let rating;
 
-  // render movie found from search
+  // Render movie found from search
   function renderMovie(movie, avgRating) {
     id = movie._id;
     let title = movie.title;
@@ -54,7 +54,7 @@ $(document).ready(function() {
     id = review.id;
     reviewText = review.content;
     rating = review.rating;
-    let date = review.Date.slice(0,10);
+    let date = review.date.slice(0,10);
 
     let reviewTemplate =
     `<div class="reviewSection">
@@ -79,10 +79,13 @@ $(document).ready(function() {
   }
 
   // Render all reviews
-  function renderAllReviews(ratingsReviews) {
-    reviewArr = ratingsReviews.reviews;
+  function renderAllReviews(reviewsRatings) {
+    reviewArr = reviewsRatings.reviews;
+    console.log(`this is renderAllReviews: ${reviewArr}`)
 
-    reviewArr.forEach(renderReview(review));
+    reviewArr.forEach(function(review){
+      renderReview(review);
+    });
   }
 
 // Collect all review data from the movie record
@@ -103,7 +106,7 @@ function getReviewData(movie) {
       });
       ratingArr.push(thisReview.rating);
     })
-    }
+  }
 
     // Handle empty reviews conditions
     if (allReviews === undefined || allReviews.length === 0) {
@@ -137,106 +140,9 @@ function getReviewData(movie) {
 
     getReviewData(movie);
     calculateAverageRating(reviewsRatings);
-    // renderMovie(movie, avgRating);
-    // renderReview(reviewsRatings);
+    renderMovie(movie, avgRating);
+    renderAllReviews(reviewsRatings);
 
-    console.log(avgRating)
-    console.log(reviewsRatings)
-
-  }
-
-  // find and show movies
-  function showMovies(movies) {
-    movies.forEach(function(movie){
-      let movieId = movie._id
-      let allReviews = movie.reviews;
-      let reviewArr = [];
-      let ratingArr =[];
-      let avgRating = 0;
-
-      // Get review data from movie record
-      if (allReviews != undefined || allReviews.length != 0) {
-        allReviews.forEach(function(thisReview){
-          reviewArr.push({
-            'id': thisReview._id,
-            'content': thisReview.reviewText,
-            'rating': thisReview.rating,
-            'date': thisReview.createdAt,
-            'movie': thisReview.movie
-          });
-          ratingArr.push(thisReview.rating);
-        })
-
-      // Render each review
-      let eachReview = reviewArr.map(function(singleReview) {
-        let reviewTemplate =
-        `<div class="reviewSection">
-          <h5>User Review</h5>
-          <div class="reviewContent row">
-            <div class="reviewData col-8">
-              <p>Review: ${singleReview.content}</p>
-              <p>Date: ${singleReview.date.slice(0, 10)}</p>
-            </div>
-          <div class="reviewChanges col-4">
-          <!-- User changes -->
-            <button type="button" name="reviewUpdate" class="btn btn-primary reviewUpdate">Update review</button>
-            <form action="/api/reviews/${singleReview.idd}/delete" method="DELETE" data-id="${singleReview.id} class="deleteReview">
-                <button type="submit" class="btn btn-danger deleteThisReview">Delete review</button>
-              </form>
-              </div>
-            </div>
-          </div>`
-        return (reviewTemplate);
-      }).join('')
-
-
-
-      // Calculate average user rating
-      if (ratingArr.length != 0) {
-        avgRating = ((ratingArr.reduce((total, singleRating) => total + singleRating)) / ratingArr.length).toFixed(2);
-      }
-
-      // Handle no rating and no review cases
-      if (ratingArr === undefined || ratingArr.length === 0) {
-        ratingArr = "No ratings currently";
-      }
-
-      if (reviewArr === undefined || reviewArr.length === 0) {
-        eachReview = "No reviews currently";
-      }
-
-      // Render movie information
-      let movieTemplate =
-      `<div class="movieSection" data-id="${movieId}">
-        <h4>Movie information</h4>
-        <div class="movieContent row">
-          <div class="movieData">
-            <p>Title: ${movie.title}
-            <p>Director: ${movie.director}</p>
-            <p>Year: ${movie.year}</p>
-            <p>Genre: ${movie.genre}</p>
-          </div>
-          <div class="userMovieInfo">
-          <p>Average user rating: ${avgRating}</p>
-          <div class="form-group">
-            <form action="/api/movies/${movieId}/reviews" method="POST" class="reviewForm">
-                <textArea name="reviewText" class=" form-control reviewText" placeholder="Enter your review here"></textArea>
-                <input type="number" name="rating" class="form-control rating" placeholder="Select a rating from 1-5" min="1" max="5"/>
-                <input type="hidden" name="movie" value="${movieId}"/>
-                <input type="submit" name="reviewSubmit" class="btn btn-primary reviewSubmit" />
-            </form>
-          </div>
-          </div>
-        </div>
-        <div class="criticlyReviews">
-          <h5>Critic.ly Reviews</h5>
-          <p class="review">${eachReview}</p>
-        </div>
-      </div>`;
-
-      $('.movieResult').prepend(movieTemplate);
-      }
-    })
   }
 
   //Delete review
@@ -256,12 +162,12 @@ function getReviewData(movie) {
   })
 
 
-    // Ajax call to find all movies and call the showMovies function
+    // Ajax call to find all movies
     function findMovies() {
       $.ajax({
         method: 'GET',
         url: '/api/movies',
-        success: showMovies,
+        success: movies => console.log(movies),
         error: handleError
       });
     }
@@ -278,8 +184,6 @@ Submit functions
   // Movie find form submissions (name only)
   $('.movieSearch').on('submit', function(event){
     event.preventDefault();
-    console.log(this);
-    console.log($('.movieSearch').serialize())
     let movieTitle = $('#oneMovie').val();
 
     $.ajax({
